4ee8efec71ae91ca2367d28aca4b5fe030dd1d2d
m4ch374 <spiner47@gmail.com>
2025-03-08 01:09:18 +1100

    chore(aut): basic clerk setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,22 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 import React from "react";
-import { useState } from "react";
-import { Button } from "./components/ui/button";
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="dark">
-      <Button>hihihihi</Button>
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>Click on the Vite and React logos to learn more</p>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </div>
   );
 };
